773f44f30c4f1a9748db49d2bce1c6099924e5c0
Rlack97 <amalilith97@gmail.com>
2022-07-28 17:41:04 +0900

    0728

--- study/#4948.py
+++ study/#4948.py
@@ -1,5 +1,5 @@
 def prime(x):
-    for z in range(2,x):
+    for z in range(2,int(x**(1/2))):
         if x % z == 0:
             return False
     return True
@@ -9,6 +9,7 @@ while True:
     i = int(input())
     if i == 0:
         break
+
     result = 0
     for k in range(i+1,2*i+1):
         if prime(k) == True:
