797045088bd39c0455cf419679d5168b5fd642fe
Rlack97 <amalilith97@gmail.com>
2022-10-09 20:25:26 +0900

    1009

--- 장고 프로젝트들/인스타그램 클론코딩/posts/models.py
+++ 장고 프로젝트들/인스타그램 클론코딩/posts/models.py
@@ -1,3 +1,11 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    image = models.ImageField(
+        upload_to='image/',
+        blank=True,
+        null=True,
+        )
--- 장고 프로젝트들/인스타그램 클론코딩/posts/views.py
+++ 장고 프로젝트들/인스타그램 클론코딩/posts/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'posts/index.html',context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        print(request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+        flag = False
+    context={
+        'form':form,
+        'flag':flag,
+    }
+    return render(request,'posts/form.html', context)
+    
+
+def update(request,pk):
+    post = Post.objects.get(pk=pk)
+    flag = True
+    if request.method=="POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm(instance=post)
+    context = {
+        'form':form,
+        'post':post,
+        'flag':flag
+    }
+    return render(request,'posts/form.html', context)
+
+
+def delete(request,pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('posts:index')
